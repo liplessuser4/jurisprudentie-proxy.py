fb46806937b456cabcc514ac763ec2a5e6e06fe7
liplessuser4 <jeroen.intven@gmail.com>
2025-04-17 15:37:47 +0200

    Update bert_loader.py

--- bert_loader.py
+++ bert_loader.py
@@ -1,7 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
 
 def load_ner_pipeline():
-    model_name = "model_name = "GroNLP/bert-base-dutch-cased"
+    model_name = "GroNLP/bert-base-dutch-cased"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForTokenClassification.from_pretrained(model_name)
     return pipeline("ner", model=model, tokenizer=tokenizer, grouped_entities=True)
